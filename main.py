662ed07cb93ad9523ca8494d860a39394ab502fc
Tashinga Masawi <blessingmasawiofficial@gmail.com>
2025-08-21 14:18:32 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# Constants (conventionally in uppercase)
+PI = 3.14159
+TAX_RATE = 0,2
+DEFAULT_VALUE = None
+
+# Variables
+name = "Blessing Tashinga Masawi"
+age = 21
+Gender = "Male"
+height = 1.62
+weight = 78.47
+skin_color = "black"
+nationality = "Zimbabwean"
+occupation = "Student"
+university = "Catholic University of Zimbabwe"
+marital_status = "Single"
+language = "English & Shona"
+favourite_language = "English"
+hobby = "programming"
+favourite_color = "Green"
+blood_type = "O plus "
+
+# Using print function to display information
+print("Personal Information:")
+print("Name:",name)
+print("Age: 21")
+print("Height:height","1.62m")
+print("Weight:weight","78.47")
+print("Skin:",skin_color)
+print("Occupation:",occupation)
+print("University:",university)
+print("Maritial Status:",marital_status)
+print("Language:",language)
+print("Nationality:",nationality)
+print("favourite language:",favourite_language)
+print("hobby:",hobby)
+print("favourite color:",favourite_color)
+print("blood type:",blood_type)
+
